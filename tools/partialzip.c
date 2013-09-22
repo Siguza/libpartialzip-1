affae3c5eece2c58dfd3b49c1aca61926ccacfd5
Joshua Hill <posixninja@crippy-dev.com>
2013-09-22 13:12:07 -0700

    added and refactored code for libpartialzip

--- tools/partialzip.c
+++ tools/partialzip.c
@@ -1,8 +1,50 @@
-/*
- * partialzip.c
- *
- *  Created on: Sep 4, 2013
- *      Author: posixninja
- */
+/**
+  * libpartialzip-1.0 - partialzip.c
+  * Copyright (C) 2010 David Wang
+  *
+  * Modified by:
+  * Copyright (C) 2010-2013 Joshua Hill
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <libcrippy-1.0/libcrippy.h>
+#include <libpartialzip-1.0/libpartialzip.h>
 
 
+void download_callback(partialzip_info_t* info, partialzip_cd_file_t* file, size_t progress) {
+	if (progress_callback)
+		progress_callback(progress, user_object);
+	}
+}
+
+
+void usage() {
+	printf("usage: partialzip <zip> <path> <output>\n");
+}
+
+int main(int argc, char* argv[]) {
+	if(argc != 4) {
+		return -1;
+	}
+	int x = partialzip_download_file(argv[1], argv[2], argv[3], (partialzip_progress_callback_t)print_progress);
+	if(x < 0) {
+		printf("Unable to download file\n");
+		return -1;
+	}
+	return 0;
+}
